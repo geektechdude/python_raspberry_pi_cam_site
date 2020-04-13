a65b042dfa6607b18058f8e23ce408c358e72ebd
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2020-04-13 18:09:00 +0100

    Update camera_flask.py

--- camera_flask.py
+++ camera_flask.py
@@ -1,27 +1,28 @@
 # geektechstuff
-
 # Libraries to import
 from flask import Flask, render_template
 from picamera import PiCamera
-from time import sleep
 from datetime import datetime
+import shutil
+import os
 
 app = Flask(__name__,static_url_path='',static_folder='/home/pi/flask_photo/')
-
-def time_now():
-    time_now = datetime.now().strftime("%H%M%Y%m%d")
-    return(time_now)
 
 def save_photo():
     camera = PiCamera()
     filename = datetime.now().strftime("%H%M%Y%m%d.jpg")
     camera.capture(filename)
     camera.close()
-    sleep(2)
-    return(filename)
+    shutil.copyfile(filename,'/home/pi/flask_photo/latest.jpg')
+    new_location = "/home/pi/flask_photo/old_images/"+filename
+    shutil.copyfile(filename, new_location)
+    if os.path.exists(filename):
+        os.remove(filename)
+    return()
 
 @app.route('/')
 def load_page():
-    filename = save_photo()
-    time = time_now()
-    return render_template("flask_temp.html",filename=filename,time=time)
+    save_photo()
+    time_hour_min = datetime.now().strftime("%H:%M")
+    time_date = datetime.now().strftime("%d-%m-%y")
+    return render_template("flask_temp.html", time_hour_min=time_hour_min, time_date=time_date)
